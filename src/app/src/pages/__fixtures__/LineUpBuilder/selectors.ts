d382641b8a55e65a84d8c127ab777294d47fb7ce
Matt Stannett <matt.stannett@gmail.com>
2026-02-26 20:32:48 +1300

    test(app): add selector constants to help with magic strings

---
+++ src/app/src/pages/__fixtures__/LineUpBuilder/selectors.ts
@@ -0,0 +1,14 @@
+export const SELECTORS = {
+  PAGE_TITLE: /line up builder/i,
+  NATION_UNITED_STATES: /united states/i,
+  NATION_GERMANY: /germany/i,
+  CATEGORY_AVIATION: /aviation/i,
+  CATEGORY_GROUND: /ground vehicles/i,
+  GAME_MODE_ARCADE: /arcade/i,
+  RESET_BUTTON: /reset filters/i,
+  VEHICLE_P51: /p-51 mustang/i,
+  ROLE_FIGHTER: /fighter/i,
+  ROLE_BOMBER: /bomber/i,
+  ROLE_STRIKE_AIRCRAFT: /strike aircraft/i,
+  BR_RANGE_GROUP: /battle rating range/i,
+} as const;
